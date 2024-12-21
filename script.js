a9a1f882381013bc85c6e4431326ef3295e67ded
Augustofabg <augustofabg@gmail.com>
2024-12-21 10:03:31 -0300

    Commit - alert number min

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 function sortear() {
+  const text2 = document.querySelector(".text2");
   const min = Math.ceil(document.querySelector(".inpt1").value);
   const max = Math.floor(document.querySelector(".inpt2").value);
   const result = Math.floor(Math.random() * (max - min) + min);
   const number = document.querySelector("span");
   number.innerHTML = result;
+  text2.style.display = "flex";
+  
+  if (min > max) {
+  const text2 = document.querySelector(".text2");
+  number.innerHTML = "O segundo numero deve ser maior que o primeiro";
+  text2.style.display = "none";
+  number.style.fontSize = "24px";
+  number.style.paddingBottom = "19px";
+  number.style.paddingTop = "16px";
+  number.style.paddingRight = "5px";
+  number.style.paddingLeft = "5px";
+}
 
   displayviwer();
 }
